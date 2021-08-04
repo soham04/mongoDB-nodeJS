132a4bb67e01597c2c8a8e01228cb6f2a065c6fe
Soham Shinde <sohamshinde04@gmail.com>
2021-08-04 12:27:21 +0530

    Added data validation

--- app.js
+++ app.js
@@ -8,8 +8,15 @@ mongoose.connect("mongodb://localhost:27017/fruitDB", {
 });
 
 const fruitSchema = new mongoose.Schema({
-  name: String,
-  rating: Number,
+  name: {
+    type: String,
+    required: [true, "Please check your data entry, no name specified"],
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 10,
+  },
   review: String,
 });
 
